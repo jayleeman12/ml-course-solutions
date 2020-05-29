18b20f08334f944e00b8f75da835c51c1809818d
jay leeman <jayleeman12@gmail.com>
2020-05-29 21:42:28 +0300

    WIP ex6

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,7 +22,22 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
-
+possible_values = [0.01,0.03,0.1,0.3,1,3,10,30];
+minimum_value = intmax;
+for i = 1:length(possible_values)
+    possible_C = possible_values(i);
+    for j = 1:length(possible_values)
+        possible_sigma = possible_values(j);
+        model = svmTrain(X, y, possible_C, @(x1, x2) gaussianKernel(x1, x2, possible_sigma));
+        predictions = svmPredict(model, Xval);
+        prediction_error = mean(double(predictions ~= yval));
+        if (prediction_error < minimum_value)
+            minimum_value = prediction_error;
+            C = possible_C;
+            sigma = possible_sigma;
+        end
+    end
+end
 
 
 
